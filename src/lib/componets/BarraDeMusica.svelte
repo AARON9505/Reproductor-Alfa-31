52cb28b276683666d560f1e5dddac44968036eb1
Aaron Gil <aaronabdiel09@gmail.com>
2026-09-25 14:49:35 -0400

    barra

---
+++ src/lib/componets/BarraDeMusica.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { actualizarSong, trackState } from "$lib/store/cancion.svelte";
+
+</script>
+
+{#if trackState.song}
+<footer class="bg-indigo-600 text-emerald-50 sticky bottom-0 px-10 py-4 flex items-center gap-4">
+
+	<div class="w-20 h-20">
+	<img src="{trackState.song.album.image}" alt="">
+    </div>
+
+    <div>
+	<p>{trackState.song.title}</p>
+	<p>{trackState.song.album.title}</p>
+    </div>
+
+    <div>
+        <audio src="{trackState.song.audio}" controls></audio>
+    </div>
+
+</footer>
+	
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,27 +2,14 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
     import { actualizarSong, trackState } from '$lib/store/cancion.svelte';
-
+    import BarraDeMusica from '$lib/componets/BarraDeMusica.svelte';
+	
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
 
-{#if trackState.song}
-<footer class="bg-indigo-400 sticky bottom-0 px-10 py-4 flex items-center gap-4">
 
-	<div class="w-20 h-20">
-	<img src="{trackState.song.album.image}" alt="">
-    </div>
-
-    <div>
-	<p>{trackState.song.title}</p>
-	<p>{trackState.song.album.title}</p>
-    </div>
-
-</footer>
-	
-{/if}
-
+<BarraDeMusica />
 
